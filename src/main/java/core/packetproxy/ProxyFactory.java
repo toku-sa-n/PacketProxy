7c613083ce6a424489a7a27214d2f97f92dfd230
Hiroki Tokunaga <hiroki.tokunaga@dena.com>
2025-12-19 21:05:38 +0900

    Revert: non-SinglePacketAttackController changes

--- src/main/java/core/packetproxy/ProxyFactory.java
+++ src/main/java/core/packetproxy/ProxyFactory.java
@@ -67,8 +67,7 @@ public class ProxyFactory {
 			listen_socket.setReuseAddress(true);
 			proxy = new ProxyXmppSSLForward(listen_socket, listen_info);
 
-		} else {
-			/* FORWARDER */
+		} else { /* FORWARDER */
 
 			ServerSocket listen_socket = new ServerSocket(listen_info.getPort());
 			listen_socket.setReuseAddress(true);
